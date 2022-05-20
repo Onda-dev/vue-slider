2413a3910cb80403a1b846dbe4f25cef479af7d3
Onda-dev <manuelaltieri3@gmail.com>
2022-05-20 14:56:37 +0200

    Vue inserito

--- js/main.js
+++ js/main.js
@@ -4,30 +4,35 @@
 // 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 // 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 
-const images = [
-    {
-        src: 'img/01.jpg',
-        title: 'Svezia',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    },
-    {
-        src: 'img/02.jpg',
-        title: 'Svizzera',
-        text: 'Lorem ipsum',
-    },
-    {
-        src: 'img/03.jpg',
-        title: 'Gran Bretagna',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    },
-    {
-        src: 'img/04.jpg',
-        title: 'Germania',
-        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-    {
-        src: 'img/05.jpg',
-        title: 'Paradise',
-        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    },
-];
+const app = new Vue({
+    el: "#root",
+    data: {
+        images: [
+            {
+                src: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                src: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum',
+            },
+            {
+                src: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                src: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                src: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+    }
+})
